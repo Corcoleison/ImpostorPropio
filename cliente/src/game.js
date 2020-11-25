d857fb953d59ba7285e1dab030e49329e881f3b1
PABLO CORCOLES MOLINA <Pablo.Corcoles2@alu.uclm.es>
2020-11-25 19:50:53 +0100

    Cambio de sprites de personaje y de tiltedmap del mapa

--- cliente/src/game.js
+++ cliente/src/game.js
@@ -37,7 +37,10 @@ function preload() {
   //  https://labs.phaser.io/view.html?src=src/animation/texture%20atlas%20animation.js
   // If you don't use an atlas, you can do the same thing with a spritesheet, see:
   //  https://labs.phaser.io/view.html?src=src/animation/single%20sprite%20sheet.js
-  this.load.atlas("atlas", "cliente/assets/atlas/atlas.png", "cliente/assets/atlas/atlas.json");
+  //this.load.atlas("atlas", "cliente/assets/atlas/atlas.png", "cliente/assets/atlas/atlas.json");
+  this.load.spritesheet("rojo","cliente/assets/images/personajeRojo.png",{frameWidth:32,frameHeight:32});
+  
+  // repetir por cada personaje diferente
 }
 
 function create() {
@@ -65,61 +68,109 @@ function create() {
 
   // Create a sprite with physics enabled via the physics system. The image used for the sprite has
   // a bit of whitespace, so I'm using setSize & setOffset to control the size of the player's body.
-  player = this.physics.add
-    .sprite(spawnPoint.x, spawnPoint.y, "atlas", "misa-front")
-    .setSize(30, 40)
-    .setOffset(0, 24);
+  //player = this.physics.add
+  //  .sprite(spawnPoint.x, spawnPoint.y, "atlas", "misa-front")
+  //  .setSize(30, 40)
+  //  .setOffset(0, 24);
+
+  player = this.physics.add.sprite(spawnPoint.x, spawnPoint.y,"rojo");
 
   // Watch the player and worldLayer for collisions, for the duration of the scene:
   this.physics.add.collider(player, worldLayer);
 
   // Create the player's walking animations from the texture atlas. These are stored in the global
   // animation manager so any sprite can access them.
+  // const anims = this.anims;
+  // anims.create({
+  //   key: "misa-left-walk",
+  //   frames: anims.generateFrameNames("atlas", {
+  //     prefix: "misa-left-walk.",
+  //     start: 0,
+  //     end: 3,
+  //     zeroPad: 3
+  //   }),
+  //   frameRate: 10,
+  //   repeat: -1
+  // });
+  // anims.create({
+  //   key: "misa-right-walk",
+  //   frames: anims.generateFrameNames("atlas", {
+  //     prefix: "misa-right-walk.",
+  //     start: 0,
+  //     end: 3,
+  //     zeroPad: 3
+  //   }),
+  //   frameRate: 10,
+  //   repeat: -1
+  // });
+  // anims.create({
+  //   key: "misa-front-walk",
+  //   frames: anims.generateFrameNames("atlas", {
+  //     prefix: "misa-front-walk.",
+  //     start: 0,
+  //     end: 3,
+  //     zeroPad: 3
+  //   }),
+  //   frameRate: 10,
+  //   repeat: -1
+  // });
+  // anims.create({
+  //   key: "misa-back-walk",
+  //   frames: anims.generateFrameNames("atlas", {
+  //     prefix: "misa-back-walk.",
+  //     start: 0,
+  //     end: 3,
+  //     zeroPad: 3
+  //   }),
+  //   frameRate: 10,
+  //   repeat: -1
+  // });
+
   const anims = this.anims;
-  anims.create({
-    key: "misa-left-walk",
-    frames: anims.generateFrameNames("atlas", {
-      prefix: "misa-left-walk.",
-      start: 0,
-      end: 3,
-      zeroPad: 3
-    }),
-    frameRate: 10,
-    repeat: -1
-  });
-  anims.create({
-    key: "misa-right-walk",
-    frames: anims.generateFrameNames("atlas", {
-      prefix: "misa-right-walk.",
-      start: 0,
-      end: 3,
-      zeroPad: 3
-    }),
-    frameRate: 10,
-    repeat: -1
-  });
-  anims.create({
-    key: "misa-front-walk",
-    frames: anims.generateFrameNames("atlas", {
-      prefix: "misa-front-walk.",
-      start: 0,
-      end: 3,
-      zeroPad: 3
-    }),
-    frameRate: 10,
-    repeat: -1
-  });
-  anims.create({
-    key: "misa-back-walk",
-    frames: anims.generateFrameNames("atlas", {
-      prefix: "misa-back-walk.",
-      start: 0,
-      end: 3,
-      zeroPad: 3
-    }),
-    frameRate: 10,
-    repeat: -1
-  });
+      anims.create({
+        key: "rojo-left-walk",
+        frames: anims.generateFrameNames("rojo", {
+          //prefix: "misa-left-walk.",
+          start: 8,
+          end: 11,
+          //zeroPad: 3
+        }),
+        //frameRate: 10,
+        repeat: -1
+      });
+      anims.create({
+        key: "rojo-right-walk",
+        frames: anims.generateFrameNames("rojo", {
+          //prefix: "misa-left-walk.",
+          start: 12,
+          end: 15,
+          //zeroPad: 3
+        }),
+        //frameRate: 10,
+        repeat: -1
+      });
+      anims.create({
+        key: "rojo-front-walk",
+        frames: anims.generateFrameNames("rojo", {
+          //prefix: "misa-left-walk.",
+          start: 0,
+          end: 3,
+          //zeroPad: 3
+        }),
+        //frameRate: 10,
+        repeat: -1
+      });
+      anims.create({
+        key: "rojo-back-walk",
+        frames: anims.generateFrameNames("rojo", {
+          //prefix: "misa-left-walk.",
+          start: 4,
+          end: 7,
+          //zeroPad: 3
+        }),
+        //frameRate: 10,
+        repeat: -1
+      });
 
   const camera = this.cameras.main;
   camera.startFollow(player);
@@ -128,32 +179,33 @@ function create() {
   cursors = this.input.keyboard.createCursorKeys();
 
   // Help text that has a "fixed" position on the screen
-  this.add
-    .text(16, 16, 'Arrow keys to move\nPress "D" to show hitboxes', {
-      font: "18px monospace",
-      fill: "#000000",
-      padding: { x: 20, y: 10 },
-      backgroundColor: "#ffffff"
-    })
-    .setScrollFactor(0)
-    .setDepth(30);
+  // this.add
+  //   .text(16, 16, 'Arrow keys to move\nPress "D" to show hitboxes', {
+  //     font: "18px monospace",
+  //     fill: "#000000",
+  //     padding: { x: 20, y: 10 },
+  //     backgroundColor: "#ffffff"
+  //   })
+  //   .setScrollFactor(0)
+  //   .setDepth(30);
 
-  // Debug graphics
-  this.input.keyboard.once("keydown_D", event => {
-    // Turn on physics debugging to show player's hitbox
-    this.physics.world.createDebugGraphic();
+  // // Debug graphics
+  // this.input.keyboard.once("keydown_D", event => {
+  //   // Turn on physics debugging to show player's hitbox
+  //   this.physics.world.createDebugGraphic();
 
-    // Create worldLayer collision graphic above the player, but below the help text
-    const graphics = this.add
-      .graphics()
-      .setAlpha(0.75)
-      .setDepth(20);
-    worldLayer.renderDebug(graphics, {
-      tileColor: null, // Color of non-colliding tiles
-      collidingTileColor: new Phaser.Display.Color(243, 134, 48, 255), // Color of colliding tiles
-      faceColor: new Phaser.Display.Color(40, 39, 37, 255) // Color of colliding face edges
-    });
-  });
+  // //   // Create worldLayer collision graphic above the player, but below the help text
+  // //   const graphics = this.add
+  // //     .graphics()
+  // //     .setAlpha(0.75)
+  // //     .setDepth(20);
+  // //   worldLayer.renderDebug(graphics, {
+  // //     tileColor: null, // Color of non-colliding tiles
+  // //     collidingTileColor: new Phaser.Display.Color(243, 134, 48, 255), // Color of colliding tiles
+  // //     faceColor: new Phaser.Display.Color(40, 39, 37, 255) // Color of colliding face edges
+  // //   });
+
+  // });
 }
 
 function update(time, delta) {
@@ -182,20 +234,20 @@ function update(time, delta) {
 
   // Update the animation last and give left/right animations precedence over up/down animations
   if (cursors.left.isDown) {
-    player.anims.play("misa-left-walk", true);
+    player.anims.play("rojo-left-walk", true);
   } else if (cursors.right.isDown) {
-    player.anims.play("misa-right-walk", true);
+    player.anims.play("rojo-right-walk", true);
   } else if (cursors.up.isDown) {
-    player.anims.play("misa-back-walk", true);
+    player.anims.play("rojo-back-walk", true);
   } else if (cursors.down.isDown) {
-    player.anims.play("misa-front-walk", true);
+    player.anims.play("rojo-front-walk", true);
   } else {
     player.anims.stop();
 
     // If we were moving, pick and idle frame to use
-    if (prevVelocity.x < 0) player.setTexture("atlas", "misa-left");
-    else if (prevVelocity.x > 0) player.setTexture("atlas", "misa-right");
-    else if (prevVelocity.y < 0) player.setTexture("atlas", "misa-back");
-    else if (prevVelocity.y > 0) player.setTexture("atlas", "misa-front");
+    if (prevVelocity.x < 0) player.setTexture("rojo", "rojo-left-walk");
+    else if (prevVelocity.x > 0) player.setTexture("rojo", "rojo-right-walk");
+    else if (prevVelocity.y < 0) player.setTexture("rojo", "rojo-back-walk");
+    else if (prevVelocity.y > 0) player.setTexture("rojo", "rojo-front-walk");
   }
 }
